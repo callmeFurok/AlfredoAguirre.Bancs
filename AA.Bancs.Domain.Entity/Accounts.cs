963c90b952adcf6fba407e63191c0d324b4f0d33
Alfredo Aguirre <thefurok@gmail.com>
2023-07-01 21:19:56 -0500

    Entidad de cuentas

---
+++ AA.Bancs.Domain.Entity/Accounts.cs
@@ -0,0 +1,11 @@
+﻿namespace AA.Bancs.Domain.Entity
+{
+    public class Accounts
+    {
+        public Guid AccountId { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountType { get; set; }
+        public string InitialBalance { get; set; }
+        public string Status { get; set; }
+    }
+}
