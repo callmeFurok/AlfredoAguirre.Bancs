7dcd84010cef3556ac69fc975a014a27aba86975
Alfredo Aguirre <thefurok@gmail.com>
2023-07-01 21:19:41 -0500

    Creadas las entidades para los clientes

---
+++ AA.Bancs.Domain.Entity/Clients.cs
@@ -0,0 +1,11 @@
+﻿namespace AA.Bancs.Domain.Entity
+{
+    public class Clients : Persons
+    {
+        public Guid ClientId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ AA.Bancs.Domain.Entity/Persons.cs
@@ -0,0 +1,14 @@
+﻿namespace AA.Bancs.Domain.Entity
+{
+    public class Persons
+    {
+        public Guid PersonId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public string IdentificationCard { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
