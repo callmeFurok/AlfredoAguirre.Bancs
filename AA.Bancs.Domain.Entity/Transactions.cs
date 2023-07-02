e100dc1b458c0ce072ce2d557683f08daeb3915f
Alfredo Aguirre <thefurok@gmail.com>
2023-07-01 21:20:08 -0500

    Entidad de movimientos

---
+++ AA.Bancs.Domain.Entity/Transactions.cs
@@ -0,0 +1,11 @@
+﻿namespace AA.Bancs.Domain.Entity
+{
+    public class Transactions
+    {
+        public Guid TransactionId { get; set; }
+        public string Date { get; set; }
+        public string TransactionType { get; set; }
+        public string Amount { get; set; }
+        public string Balance { get; set; }
+    }
+}
