a78286f3cc140803825cb27360caf54f33c4a4e3
Alfredo Aguirre <thefurok@gmail.com>
2023-07-01 21:20:57 -0500

    DbContext para uso de Entity Framewrok

---
+++ AA.Bancs.Infraestructure.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AA.Bancs.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+namespace AA.Bancs.Infraestructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Modelos de la base de datos
+        public DbSet<Accounts> Accounts { get; set; }
+        public DbSet<Clients> Clients { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+    }
+}
