c018f418f50347131d606fec70250c02981845b7
Alfredo Aguirre <thefurok@gmail.com>
2023-07-03 00:34:01 -0500

    Capa de aplicaciones para Clientes terminada v1

---
+++ AA.Bancs.Application.Interface/IClientsApplication.cs
@@ -0,0 +1,18 @@
+﻿using AA.Bancs.Applications.DTO;
+using AA.Bancs.Transversal.Commom;
+
+namespace AA.Bancs.Application.Interface
+{
+    public interface IClientsApplication
+    {
+        Task<Response<bool>> InsertAsync(ClientsDto clientDto);
+
+        Task<Response<bool>> UpdateAsync(ClientsDto clientDto);
+
+        Task<Response<bool>> DeleteAsync(Guid clientId);
+
+        Task<Response<ClientsDto>> GetClientByIdAsync(Guid clientId);
+
+        Task<Response<IEnumerable<ClientsDto>>> GetAllClientsAsync();
+    }
+}
