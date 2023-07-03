205b718f1b4ad77962ab96ce64e566a5b2fe8336
Alfredo Aguirre <thefurok@gmail.com>
2023-07-03 00:34:30 -0500

    Dto para clientes general v1

---
+++ AA.Bancs.Applications.DTO/ClientsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace AA.Bancs.Applications.DTO
+{
+    public class ClientsDto
+    {
+        public Guid ClientId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public string IdentificationCard { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+    }
+}
