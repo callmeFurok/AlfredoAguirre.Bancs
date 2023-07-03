61ab4b719995ac9deb64e34fb9660f2608d7721b
Alfredo Aguirre <thefurok@gmail.com>
2023-07-03 00:39:39 -0500

    proyecto para el uso del automapper

---
+++ AA.Bancs.Transversal.Mapp/MappingsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AA.Bancs.Applications.DTO;
+using AA.Bancs.Domain.Entity;
+using AutoMapper;
+
+namespace AA.Bancs.Transversal.Mapp
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            //mapeo de reverso
+            CreateMap<Clients, ClientsDto>().ReverseMap();
+
+        }
+    }
+}
