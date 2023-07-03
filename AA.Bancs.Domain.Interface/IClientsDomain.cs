75e38cf88d259b759f124d07a6af49dc4b779a4b
Alfredo Aguirre <thefurok@gmail.com>
2023-07-03 00:35:50 -0500

    Capa de interfaces

---
+++ AA.Bancs.Domain.Interface/IClientsDomain.cs
@@ -0,0 +1,17 @@
+﻿using AA.Bancs.Domain.Entity;
+
+namespace AA.Bancs.Domain.Interface
+{
+    public interface IClientsDomain
+    {
+        Task<bool> InsertAsync(Clients client);
+
+        Task<bool> UpdateAsync(Clients client);
+
+        Task<bool> DeleteAsync(Guid clientId);
+
+        Task<Clients> GetClientByIdAsync(Guid clientId);
+
+        Task<IEnumerable<Clients>> GetAllClientsAsync();
+    }
+}
