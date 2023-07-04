5d4e6f8532e4645a1c840f75a8f48417da1d1a77
Alfredo Aguirre <thefurok@gmail.com>
2023-07-04 15:11:54 -0500

    Interfaz de las transacciones capa Aplicacion

---
+++ WSApplication.Interface/ITransactionsApplication.cs
@@ -0,0 +1,20 @@
+﻿using WSApplication.DTO;
+using WSDomain.Entity;
+using WSTransversal.Commom;
+
+namespace WSApplication.Interface
+{
+    public interface ITransactionsApplication
+    {
+        Task<Response<bool>> InsertAsync(TransactionsDto transactionsDto);
+
+        Task<Response<bool>> UpdateAsync(TransactionsDto transactionsDto);
+
+        Task<Response<bool>> DeleteAsync(Guid transactionId);
+
+        Task<Response<Transactions>> GetTransactionsByIdAsync(Guid transactionId);
+
+        Task<Response<IEnumerable<Transactions>>> GetAllTransactionsAsync(Guid accountId);
+        Task<Response<IEnumerable<Transactions>>> GetTransactionsReport(DateTime startDate, DateTime endDate);
+    }
+}
